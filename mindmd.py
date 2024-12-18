17df056daf10315a342288b77763030fd892ede0
Don Sudduth <don.sudduth@gmail.com>
2024-12-18 11:27:06 -0600

    extract zip files individually with retries

--- mindmd.py
+++ mindmd.py
@@ -469,15 +469,15 @@ def format_map(parent_value, tree_nodes, a, ee, level, numbered, infile, outfile
                                 a.append("\t"*(level+1) + "- ![](" + media_path + mfile + ")\n")
                                 #e.append(str(node.parent) + "," + str(media_path + mfile) + "," + "i")
                                 #media
-                                for attempt in range(1, 3):
+                                for attempt in range(1, 11):
                                     try:
                                         shutil.copy2("images/" + mfile, out_path + media_path + mfile)
                                     except Exception as e:
                                         #print ("Image copy error: " + str(e))
-                                        if attempt == 2:
+                                        if attempt == 10:
                                             print ("Image file 'images/" + mfile + "' missing or not accessible!!")
                                             continue
-                                        time.sleep(0.25)
+                                        time.sleep(0.5)
                             #check filecount in images matches file count 
 
 
